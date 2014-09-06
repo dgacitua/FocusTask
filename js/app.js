7adfd725ed1fa750820b46d9059212ac39e4c2d4
Daniel Gacitua <gacix@riseup.net>
2014-09-06 05:12:03 -0400

    Ayuda y About agregados

--- js/app.js
+++ js/app.js
@@ -15,6 +15,14 @@ document.querySelector('#btn-viewtask').addEventListener ('click', function () {
   document.querySelector('#viewtask').className = 'current';
   document.querySelector('[data-position="current"]').className = 'left';
 });
+document.querySelector('#btn-about').addEventListener ('click', function () {
+  document.querySelector('#about').className = 'current';
+  document.querySelector('[data-position="current"]').className = 'left';
+});
+document.querySelector('#btn-help').addEventListener ('click', function () {
+  document.querySelector('#help').className = 'current';
+  document.querySelector('[data-position="current"]').className = 'left';
+});
 
 
 document.querySelector('#btn-pomo-back').addEventListener ('click', function () {
@@ -34,6 +42,15 @@ document.querySelector('#btn-viewtask-back').addEventListener ('click', function
   document.querySelector('#viewtask').className = 'right';
   document.querySelector('[data-position="current"]').className = 'current';
 });
+document.querySelector('#btn-help-back').addEventListener ('click', function () {
+  document.querySelector('#help').className = 'right';
+  document.querySelector('[data-position="current"]').className = 'current';
+});
+document.querySelector('#btn-about-back').addEventListener ('click', function () {
+  document.querySelector('#about').className = 'right';
+  document.querySelector('[data-position="current"]').className = 'current';
+});
+
 
 //action menu
 document.querySelector('#btn-action-menu').addEventListener ('click', function () {
